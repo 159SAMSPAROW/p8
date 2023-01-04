b583a53026d9a5a662b6839c248db2c537856dc5
Sami DEMIR <96701580+159SAMSPAROW@users.noreply.github.com>
2023-01-04 16:44:54 +0100

    add callToAction component

--- client/src/components/CallToAction.jsx
+++ client/src/components/CallToAction.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const CallToAction = () => {
   return (
-    <div>CallToAction</div>
+    <div className='gpt3__callToAction'>
+      <div className='gpt3__callToAction-content'>
+        <p>Request Early Access to get Started</p>
+        <h3>Register today & start exploring</h3>
+      </div>
+      <div className='gpt3__callToAction-btn'>
+        <button type='button'>Get Started</button>
+      </div>
+    </div>
   )
 }
 
